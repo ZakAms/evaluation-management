9f46a0584e3f9bbdf9aeff22406df30c37cd2cd8
zak <zakams@gmail.com>
2023-05-05 18:37:03 +0300

    add db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,33 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuizModule } from './quiz/quiz.module';
 import { CategoriesModule } from './categories/categories.module';
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { configValidationSchema } from './config.schema';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [QuizModule, CategoriesModule],
+  imports: [
+    ConfigModule.forRoot({      
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+      validationSchema: configValidationSchema,
+    }),
+    QuizModule, 
+    CategoriesModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory:  (configService: ConfigService) => ({
+        type: "postgres",
+        autoLoadEntities: true,
+        synchronize: true,
+        host: configService.get("DB_HOST"),
+        port: configService.get("DB_PORT"),
+        username: configService.get("DB_USERNAME"),
+        password: configService.get("DB_PASSWORD"),
+        database: configService.get("DB_DATABASE"),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
